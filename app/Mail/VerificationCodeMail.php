ca6fef55f4cbeb36aabfa37d8610fd3029a62f63
Emad Ghafari <emadghafari@users.noreply.github.com>
2026-03-09 06:49:24 +0200

    fix(mail): force add missing VerificationCodeMail.php

---
+++ app/Mail/VerificationCodeMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $code;
+
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(subject: 'Your iFuture Verification Code');
+    }
+
+    public function content(): Content
+    {
+        return new Content(view: 'emails.verification_code');
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
